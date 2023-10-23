fbfcd674f3c094217b443e2f83159e42a20198fb
yaimai289 <145836126+yaimai289@users.noreply.github.com>
2023-10-23 15:45:49 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
-# include <stdio.h>
+# include<stdio.h>
+
+int hello();
+int world();
 
 int main(){
-    printf("Hello ");
+    hello();
+    world();
+    printf("This is main\n");
     return 0;
 }
